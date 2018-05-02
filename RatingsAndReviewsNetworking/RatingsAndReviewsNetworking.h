374229aadea67ee35f4600d3b8f2a15ca4b340ea
Maqueda <jjmaquedaf@liverpool.com.mx>
2018-05-02 01:07:40 -0500

    Proyecto de Framework para Ratings and Reviews inicializado

---
+++ RatingsAndReviewsNetworking/RatingsAndReviewsNetworking.h
@@ -0,0 +1,19 @@
+//
+//  RatingsAndReviewsNetworking.h
+//  RatingsAndReviewsNetworking
+//
+//  Created by Josue J Maqueda Flores on 02/05/18.
+//  Copyright © 2018 Josue J Maqueda Flores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RatingsAndReviewsNetworking.
+FOUNDATION_EXPORT double RatingsAndReviewsNetworkingVersionNumber;
+
+//! Project version string for RatingsAndReviewsNetworking.
+FOUNDATION_EXPORT const unsigned char RatingsAndReviewsNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RatingsAndReviewsNetworking/PublicHeader.h>
+
+
