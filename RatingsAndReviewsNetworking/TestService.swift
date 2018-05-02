8e7a9867e9f0327b16236ea033108a959c82881f
Maqueda <jjmaquedaf@liverpool.com.mx>
2018-05-02 01:41:44 -0500

    Se creó una clase de prueba para verificar el funcionamiento del pod

---
+++ RatingsAndReviewsNetworking/TestService.swift
@@ -0,0 +1,20 @@
+//
+//  TestService.swift
+//  RatingsAndReviewsNetworking
+//
+//  Created by Josue J Maqueda Flores on 02/05/18.
+//  Copyright © 2018 Josue J Maqueda Flores. All rights reserved.
+//
+
+import Foundation
+
+public class TestService
+{
+    
+    private init() {}
+    
+    public static func doSomething()->String
+    {
+        return "Funcion de prueba del framework del pod creado"
+    }
+}
