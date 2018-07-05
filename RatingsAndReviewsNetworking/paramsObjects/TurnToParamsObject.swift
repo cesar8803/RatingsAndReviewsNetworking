e979853534111ca90f1a4e1475ce372dd84f1f45
Unknown <jjmaquedaf@liverpool.com.mx>
2018-07-05 16:31:01 -0500

    Avance Pod
    Avance la parte de  modelos para la parte de creación de review

--- RatingsAndReviewsNetworking/paramsObjects/TurnToParamsObject.swift
+++ RatingsAndReviewsNetworking/paramsObjects/TurnToParamsObject.swift
@@ -13,6 +13,9 @@ import Foundation
  de los request necesarios para el API de TurnTo
  *********************/
 
+/*********
+ Objjeto para optencion e Invalidacion de Token para API
+*******************/
 public struct TurnToObjAccessToken:TurnToGeneric
 {
     //Properties
@@ -65,9 +68,9 @@ public struct TurnToObjInvAccessToken:TurnToGeneric
     }
 }
 
-/**
+/*********
  Este aplica para el servicio de RewviewList-ProductList-UgcSummary
-*/
+*******************/
 public struct TurnToObjAskSku:TurnToGeneric
 {
     //Properties
@@ -97,16 +100,161 @@ public struct TurnToObjAskSku:TurnToGeneric
     }
 }
 
+/*********
+ Objeto para crear un review y calificar
+ *******************/
 public struct TurnToObjCreateReview:TurnToGeneric
 {
-    //Properties
+    //required
+    public var rating                       : Int
+    public var user                         : TurnToObjUser
+    public var catalogItems                 : [TurnToObjCatalogItems]
     
+    //optionals
+    public var externalId                   : String?
+    public var dateCreated                  : String?
+    public var title                        : String?
+    public var text                         : String?
+    public var locale                       : String?
+    public var acceptedTermsAndConditions   : String?
+    public var userIpAddr                   : String?
+    public var userAgent                    : String?
+    public var dimensions                   : [TurnToObjDimensions]?
+    public var media                        : TurnToObjMedia?
+    public var turntoTrackingToken          : String?
+    
+    public init(rating:Int, user:TurnToObjUser, catalogItems:[TurnToObjCatalogItems])
+    {
+        self.rating = rating
+        self.user = user
+        self.catalogItems = catalogItems
+        //
+        self.externalId = nil
+        self.dateCreated = nil
+        self.title = nil
+        self.text = nil
+        self.locale = nil
+        self.acceptedTermsAndConditions = nil
+        self.userIpAddr = nil
+        self.userAgent = nil
+        self.dimensions = nil
+        self.media = nil
+        self.turntoTrackingToken = nil
+        //
+        
+    }
     
     public func getDict() -> [String : Any] {
+        
         return [:]
     }
 }
 
+public struct TurnToObjUser :TurnToGeneric
+{
+    //required
+    public var firstName        : String
+    public var lastName         : String
+    
+    //optionals
+    public var nickName         : String?
+    public var emailAddress     : String?
+    public var externalId       : String?
+    public var city             : String?
+    public var state            : String?
+    public var country          : String?
+    public var ageRange         : Int!
+    public var shopperProfiles  : [TurnToObjShopperProfiles]?
+    
+    public func getDict() -> [String : Any]
+    {
+        return [:]
+    }
+}
+
+public struct TurnToObjShopperProfiles:TurnToGeneric
+{
+    //optionals
+    public var idShopper       : Int?
+    public var value           : String?
+    
+    public func getDict() -> [String : Any]
+    {
+        return [:]
+    }
+}
+
+public struct TurnToObjDimensions:TurnToGeneric
+{
+    //optionals
+    public var valueDimension   : Int?
+    
+    public func getDict() -> [String : Any]
+    {
+        return [:]
+    }
+}
+
+public struct TurnToObjMedia:TurnToGeneric
+{
+    //optionals
+    public var ids      : [Int]?
+    public var photos   : [TurnToObjPhotos]?
+    public var videos   : [TurnToObjVideos]?
+    
+    public func getDict() -> [String : Any]
+    {
+        return [:]
+    }
+}
+
+public struct TurnToObjPhotos:TurnToGeneric
+{
+    //required
+    public var b64data      : String
+    
+    //optionals
+    public var caption      : String?
+    
+    
+    public func getDict() -> [String : Any]
+    {
+        return [:]
+    }
+}
+
+public struct TurnToObjVideos:TurnToGeneric
+{
+    //required
+    public var url          : String
+    
+    //optionals
+    public var caption      : String?
+    
+    public func getDict() -> [String : Any]
+    {
+        return [:]
+    }
+}
+
+public struct TurnToObjCatalogItems:TurnToGeneric
+{
+    //required
+    public var sku         : String
+    
+    //optionals
+    public var title       : String
+    public var url         : String
+    
+    public func getDict() -> [String : Any]
+    {
+        return [:]
+    }
+}
+
+/*********
+ Objeto para actualizar item
+ *******************/
 public struct TurnToUpdateProduct:TurnToGeneric
 {
     
